ba272a15310773e14cc49eb409819794dbbec55a
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-19 23:52:19 +0000

    refactor welcome menu

--- lib/essential_oils/cli.rb
+++ lib/essential_oils/cli.rb
@@ -4,12 +4,14 @@ class CLI
   
   def call 
     puts "Welcome to Essential Oils!"
+    puts ""
     Scraper.scrape_oils
     list_oils
+    puts ""
     input = ""
     while input != "exit" do
-      puts "Enter the number of an essential oil to see its details or 'exit' to exit."
-      inputs = gets.strip
+      puts "Enter a number to see details for that essential oil or type 'exit' to exit."
+      input = gets.strip
       # scrape page of selected oil if not already scraped
     # binding.pry
     end
