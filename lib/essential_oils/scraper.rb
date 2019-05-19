503347993e00f14440c3997c835722d5f72aea41
BethAnn Hanlon <herbivore137@gmail.com>
2019-05-19 22:10:25 +0000

    fix bug in first level scrape

--- lib/essential_oils/scraper.rb
+++ lib/essential_oils/scraper.rb
@@ -5,30 +5,25 @@ class Scraper
   def self.scrape_oils
     html = open("https://www.gardenoflife.com/content/essential-oils/")
     doc = Nokogiri::HTML(html)
-    index = 0
-    doc.css("div .single-content")[0].children.each do |element|
-      if index >= 4 && index = doc.css("div .single-content")[0].children.count - 1
-        
-        #first data column
+    oil_array = doc.css("div .single-content")[0].children[4..-4]
+    oil_array.each do |element|
+
+        #first oils column
         oil = Oil.new(element.children[1].css("h3").text)
         element.children[1].css("p").each do |p|
           oil.effects << p.text
         end
         oil.url = element.children[0].css("a").attr("href").value
         
-        #second data column
+        #second oils column
         oil = Oil.new(element.children[3].css("h3").text)
         element.children[3].css("p").each do |p|
           oil.effects << p.text
         end
         oil.url = element.children[2].css("a").attr("href").value
-        
-        binding.pry
-      else
-        index += 1
-      end
-        
+    
     end
+      
   end
   
   
@@ -38,7 +33,14 @@ class Scraper
     html = open(oil.url)
     doc = Nokogiri::HTML(html)
     index = 0
-    doc.css("div .product-content")[1].children.each do |element|
+    doc.css("div .detail")[0].children[0].each do |element|
+      binding.pry
+      # # element.css("p").each do |p|
+      
+      
+      # end
+      
+      
       
     end
     
